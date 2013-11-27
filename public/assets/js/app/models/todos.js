df68adbb23c3ad8dc42c8c50b011a7314fd96cce
Thalida <thalida.c.noel@gmail.com>
2013-11-27 11:20:34 -0500

    Tabs to spaces

--- public/assets/js/app/models/todos.js
+++ public/assets/js/app/models/todos.js
@@ -1,32 +1,32 @@
 define(function (require){
 	"use strict"
 	
-	var	$			=	require('jquery'),
-		_			= 	require('underscore'),
-		Backbone		= 	require('backbone'),
+	var     $			=	require('jquery'),
+            _			= 	require('underscore'),
+            Backbone	= 	require('backbone'),
 	    
-		Todos = Backbone.Model.extend({
-			defaults: {
-				task: null,
-				completed: false
-			},
+	        Todos = Backbone.Model.extend({
+			    defaults: {
+				    task: null,
+				    completed: false
+			    },
 			
-			initalize: function() {},
+			    initalize: function() {},
 			
-			setStatus: function(){
-				// GET COMPLETED STATUS AND TOGGLE IT
-				var isCompleted = this.get('completed');
-				this.save({completed: !isCompleted});
-			}
-		}),
+			    setStatus: function(){
+				    // GET COMPLETED STATUS AND TOGGLE IT
+				    var isCompleted = this.get('completed');
+				    this.save({completed: !isCompleted});
+			    }
+		    }),
 	
-		TodosCollection = Backbone.Collection.extend({
-			model: Todos,
-			localStorage: new Backbone.LocalStorage("godoStorage")
-		});
+		    TodosCollection = Backbone.Collection.extend({
+			    model: Todos,
+			    localStorage: new Backbone.LocalStorage("godoStorage")
+		    });
 	
 	return {
-		Todos		:	Todos,
-		TodosCollection	:	TodosCollection
+	    Todos              :	Todos,
+		TodosCollection    :	TodosCollection
 	}
 });
